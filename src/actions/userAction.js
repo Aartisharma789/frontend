475f376b3cc81d265620fce44e522b9251d4cc0b
Mohamad Anas <mohamadanas0501@gmail.com>
2024-04-18 21:16:59 +0530

    Removed old changes

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -118,7 +118,7 @@ export const loadUser = () => async (dispatch) => {
 
 		dispatch({ type: LOAD_USER_REQUEST });
 
-		const { data } = await axios.get('/api/v1/me', {withCredentials: true});
+		const { data } = await axios.get('/api/v1/me');
 
 		dispatch({
 			type: LOAD_USER_SUCCESS,

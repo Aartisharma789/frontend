80d180912c097af7b96407ccc4e4e1df798a5a1f
Mohamad Anas <mohamadanas0501@gmail.com>
2024-04-27 13:50:43 +0530

    Added New Feature to add a product with ASIN Number

--- src/components/Admin/NewProduct.jsx
+++ src/components/Admin/NewProduct.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
 import MenuItem from '@mui/material/MenuItem';
 import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 import { useSnackbar } from 'notistack';
 import { useNavigate } from 'react-router-dom';
 import { NEW_PRODUCT_RESET } from '../../constants/productConstants';
@@ -13,7 +14,6 @@ import MetaData from '../Layouts/MetaData';
 import BackdropLoader from '../Layouts/BackdropLoader';
 
 const NewProduct = () => {
-
 	const dispatch = useDispatch();
 	const { enqueueSnackbar } = useSnackbar();
 	const navigate = useNavigate();
@@ -21,50 +21,87 @@ const NewProduct = () => {
 	const { loading, success, error } = useSelector((state) => state.newProduct);
 
 	const [highlights, setHighlights] = useState([]);
-	const [highlightInput, setHighlightInput] = useState("");
+	const [highlightInput, setHighlightInput] = useState('');
 	const [specs, setSpecs] = useState([]);
 	const [specsInput, setSpecsInput] = useState({
-		title: "",
-		description: ""
+		title: '',
+		description: '',
 	});
 
-	const [name, setName] = useState("");
-	const [description, setDescription] = useState("");
+	const [name, setName] = useState('');
+	const [description, setDescription] = useState('');
 	const [price, setPrice] = useState(0);
 	const [cuttedPrice, setCuttedPrice] = useState(0);
-	const [category, setCategory] = useState("");
+	const [category, setCategory] = useState('');
 	const [stock, setStock] = useState(0);
 	const [warranty, setWarranty] = useState(0);
-	const [brand, setBrand] = useState("");
+	const [brand, setBrand] = useState('');
 	const [images, setImages] = useState([]);
 	const [imagesPreview, setImagesPreview] = useState([]);
+	const [logo, setLogo] = useState('');
+	const [logoPreview, setLogoPreview] = useState('');
 
-	const [logo, setLogo] = useState("");
-	const [logoPreview, setLogoPreview] = useState("");
+	const [asin, setAsin] = useState('');
+	const [fetchingDetails, setFetchingDetails] = useState(false);
+
+	const handleAsinChange = (e) => {
+		setAsin(e.target.value);
+	};
+
+	const fetchProductDetails = async () => {
+		setFetchingDetails(true);
+		try {
+			const responce = await axios.post('/api/v1/fetch-product', { asin });
+
+			const { amazonProductDetails } = responce.data;
+
+			console.log('Image URLs:', amazonProductDetails.imageUrl);
+
+			setName(amazonProductDetails.title);
+			setDescription(Array.isArray(amazonProductDetails.description) ? amazonProductDetails.description.join('\n') : amazonProductDetails.description);
+			setPrice(amazonProductDetails.price);
+			setCuttedPrice(amazonProductDetails.cuttedPrice);
+			setBrand(amazonProductDetails.brand);
+			setLogo(amazonProductDetails.brandImageUrl);
+			setImages([amazonProductDetails.imageUrl]);
+			setCategory(amazonProductDetails.category);
+
+			setImagesPreview([{ url: amazonProductDetails.imageUrl }]);
+
+			enqueueSnackbar('Product details fetched successfully', { variant: 'success' });
+		} catch (error) {
+			enqueueSnackbar('Failed to fetch product details', { variant: 'error' });
+			console.error(error);
+		} finally {
+			setFetchingDetails(false);
+		}
+	};
+
+	
 
 	const handleSpecsChange = (e) => {
 		setSpecsInput({ ...specsInput, [e.target.name]: e.target.value });
-	}
+	};
 
 	const addSpecs = () => {
 		if (!specsInput.title.trim() || !specsInput.title.trim()) return;
 		setSpecs([...specs, specsInput]);
-		setSpecsInput({ title: "", description: "" });
-	}
+		setSpecsInput({ title: '', description: '' });
+	};
 
 	const addHighlight = () => {
 		if (!highlightInput.trim()) return;
 		setHighlights([...highlights, highlightInput]);
-		setHighlightInput("");
-	}
+		setHighlightInput('');
+	};
 
 	const deleteHighlight = (index) => {
-		setHighlights(highlights.filter((h, i) => i !== index))
-	}
+		setHighlights(highlights.filter((h, i) => i !== index));
+	};
 
 	const deleteSpec = (index) => {
-		setSpecs(specs.filter((s, i) => i !== index))
-	}
+		setSpecs(specs.filter((s, i) => i !== index));
+	};
 
 	const handleLogoChange = (e) => {
 		const reader = new FileReader();
@@ -77,150 +114,167 @@ const NewProduct = () => {
 		};
 
 		reader.readAsDataURL(e.target.files[0]);
-	}
+	};
 
 	const handleProductImageChange = (e) => {
 		const files = Array.from(e.target.files);
 
-		setImages([]);
-		setImagesPreview([]);
-
-		files.forEach((file) => {
-			const reader = new FileReader();
-
-			reader.onload = () => {
-				if (reader.readyState === 2) {
-					setImagesPreview((oldImages) => [...oldImages, reader.result]);
-					setImages((oldImages) => [...oldImages, reader.result]);
-				}
-			}
-			reader.readAsDataURL(file);
-		});
-	}
+    const newImages = files.map((file) => URL.createObjectURL(file));
+    setImages([...images, ...newImages]);
+    
+    // Update imagesPreview if needed
+    const previews = files.map((file) => ({
+        url: URL.createObjectURL(file)
+    }));
+    setImagesPreview([...imagesPreview, ...previews]);
+	};
 
 	const newProductSubmitHandler = (e) => {
 		e.preventDefault();
 
+
+		console.log('Type of images:', typeof images);
+console.log('Value of images:', images);
+
 		// required field checks
 		if (highlights.length <= 0) {
-			enqueueSnackbar("Add Highlights", { variant: "warning" });
+			enqueueSnackbar('Add Highlights', { variant: 'warning' });
 			return;
 		}
 		if (!logo) {
-			enqueueSnackbar("Add Brand Logo", { variant: "warning" });
+			enqueueSnackbar('Add Brand Logo', { variant: 'warning' });
 			return;
 		}
 		if (specs.length <= 1) {
-			enqueueSnackbar("Add Minimum 2 Specifications", { variant: "warning" });
+			enqueueSnackbar('Add Minimum 2 Specifications', { variant: 'warning' });
 			return;
 		}
 		if (images.length <= 0) {
-			enqueueSnackbar("Add Product Images", { variant: "warning" });
+			enqueueSnackbar('Add Product Images', { variant: 'warning' });
 			return;
 		}
 
 		const formData = new FormData();
 
-		formData.set("name", name);
-		formData.set("description", description);
-		formData.set("price", price);
-		formData.set("cuttedPrice", cuttedPrice);
-		formData.set("category", category);
-		formData.set("stock", stock);
-		formData.set("warranty", warranty);
-		formData.set("brandname", brand);
-		formData.set("logo", logo);
+		formData.set('name', name);
+		formData.set('description', description);
+		formData.set('price', price);
+		formData.set('cuttedPrice', cuttedPrice);
+		formData.set('category', category);
+		formData.set('stock', stock);
+		formData.set('warranty', warranty);
+		formData.set('brandname', brand);
+		formData.set('logo', logo);
 
 		images.forEach((image) => {
-			formData.append("images", image);
+			formData.append('images', image);
 		});
 
 		highlights.forEach((h) => {
-			formData.append("highlights", h);
+			formData.append('highlights', h);
 		});
 
 		specs.forEach((s) => {
-			formData.append("specifications", JSON.stringify(s));
+			formData.append('specifications', JSON.stringify(s));
 		});
 
 		dispatch(createProduct(formData));
-	}
+	};
 
 	useEffect(() => {
 		if (error) {
-			enqueueSnackbar(error, { variant: "error" });
+			enqueueSnackbar(error, { variant: 'error' });
 			dispatch(clearErrors());
 		}
 		if (success) {
-			enqueueSnackbar("Product Created", { variant: "success" });
+			enqueueSnackbar('Product Created', { variant: 'success' });
 			dispatch({ type: NEW_PRODUCT_RESET });
-			navigate("/admin/products");
+			navigate('/admin/products');
 		}
 	}, [dispatch, error, success, navigate, enqueueSnackbar]);
 
 	return (
 		<>
-			<MetaData title="Admin: New Product | Flipkart" />
+			<MetaData title='Admin: New Product | Flipkart' />
 
+			<div className='flex justify-between items-center border rounded bg-white'>
+				<input
+					type='text'
+					placeholder='Enter ASIN Number'
+					value={asin}
+					onChange={handleAsinChange}
+					className='px-4 flex-1 outline-none border-none'
+				/>
+				<span className='py-2 px-6 bg-primary-blue text-white rounded-r hover:shadow-lg cursor-pointer'
+					onClick={fetchProductDetails}
+				>
+					Add
+				</span>
+			</div>
 			{loading && <BackdropLoader />}
-			<form onSubmit={newProductSubmitHandler} encType="multipart/form-data" className="flex flex-col sm:flex-row bg-white rounded-lg shadow p-4" id="mainform">
 
-				<div className="flex flex-col gap-3 m-2 sm:w-1/2">
+			<form
+				onSubmit={newProductSubmitHandler}
+				encType='multipart/form-data'
+				className='flex flex-col sm:flex-row bg-white rounded-lg shadow p-4'
+				id='mainform'
+			>
+				<div className='flex flex-col gap-3 m-2 sm:w-1/2'>
 					<TextField
-						label="Name"
-						variant="outlined"
-						size="small"
+						label='Name'
+						variant='outlined'
+						size='small'
 						required
 						value={name}
 						onChange={(e) => setName(e.target.value)}
 					/>
 					<TextField
-						label="Description"
+						label='Description'
 						multiline
 						rows={3}
 						required
-						variant="outlined"
-						size="small"
+						variant='outlined'
+						size='small'
 						value={description}
 						onChange={(e) => setDescription(e.target.value)}
 					/>
-					<div className="flex justify-between">
+					<div className='flex justify-between'>
 						<TextField
-							label="Price"
-							type="number"
-							variant="outlined"
-							size="small"
+							label='Price'
+							type='number'
+							variant='outlined'
+							size='small'
 							InputProps={{
 								inputProps: {
-									min: 0
-								}
+									min: 0,
+								},
 							}}
 							required
 							value={price}
 							onChange={(e) => setPrice(e.target.value)}
 						/>
 						<TextField
-							label="Cutted Price"
-							type="number"
-							variant="outlined"
-							size="small"
+							label='Cutted Price'
+							type='number'
+							variant='outlined'
+							size='small'
 							InputProps={{
 								inputProps: {
-									min: 0
-								}
+									min: 0,
+								},
 							}}
 							required
 							value={cuttedPrice}
 							onChange={(e) => setCuttedPrice(e.target.value)}
 						/>
 					</div>
-					<div className="flex justify-between gap-4">
+					<div className='flex justify-between gap-4'>
 						<TextField
-							label="Category"
+							label='Category'
 							select
 							fullWidth
-							variant="outlined"
-							size="small"
+							variant='outlined'
+							size='small'
 							required
 							value={category}
 							onChange={(e) => setCategory(e.target.value)}
@@ -232,28 +286,28 @@ const NewProduct = () => {
 							))}
 						</TextField>
 						<TextField
-							label="Stock"
-							type="number"
-							variant="outlined"
-							size="small"
+							label='Stock'
+							type='number'
+							variant='outlined'
+							size='small'
 							InputProps={{
 								inputProps: {
-									min: 0
-								}
+									min: 0,
+								},
 							}}
 							required
 							value={stock}
 							onChange={(e) => setStock(e.target.value)}
 						/>
 						<TextField
-							label="Warranty"
-							type="number"
-							variant="outlined"
-							size="small"
+							label='Warranty'
+							type='number'
+							variant='outlined'
+							size='small'
 							InputProps={{
 								inputProps: {
-									min: 0
-								}
+									min: 0,
+								},
 							}}
 							required
 							value={warranty}
@@ -261,17 +315,31 @@ const NewProduct = () => {
 						/>
 					</div>
 
-					<div className="flex flex-col gap-2">
-						<div className="flex justify-between items-center border rounded">
-							<input value={highlightInput} onChange={(e) => setHighlightInput(e.target.value)} type="text" placeholder="Highlight" className="px-2 flex-1 outline-none border-none" />
-							<span onClick={() => addHighlight()} className="py-2 px-6 bg-primary-blue text-white rounded-r hover:shadow-lg cursor-pointer">Add</span>
+					<div className='flex flex-col gap-2'>
+						<div className='flex justify-between items-center border rounded'>
+							<input
+								value={highlightInput}
+								onChange={(e) => setHighlightInput(e.target.value)}
+								type='text'
+								placeholder='Highlight'
+								className='px-2 flex-1 outline-none border-none'
+							/>
+							<span
+								onClick={() => addHighlight()}
+								className='py-2 px-6 bg-primary-blue text-white rounded-r hover:shadow-lg cursor-pointer'
+							>
+								Add
+							</span>
 						</div>
 
-						<div className="flex flex-col gap-1.5">
+						<div className='flex flex-col gap-1.5'>
 							{highlights.map((h, i) => (
-								<div className="flex justify-between rounded items-center py-1 px-2 bg-green-50">
-									<p className="text-green-800 text-sm font-medium">{h}</p>
-									<span onClick={() => deleteHighlight(i)} className="text-red-600 hover:bg-red-100 p-1 rounded-full cursor-pointer">
+								<div className='flex justify-between rounded items-center py-1 px-2 bg-green-50'>
+									<p className='text-green-800 text-sm font-medium'>{h}</p>
+									<span
+										onClick={() => deleteHighlight(i)}
+										className='text-red-600 hover:bg-red-100 p-1 rounded-full cursor-pointer'
+									>
 										<DeleteIcon />
 									</span>
 								</div>
@@ -279,81 +347,120 @@ const NewProduct = () => {
 						</div>
 					</div>
 
-					<h2 className="font-medium">Brand Details</h2>
-					<div className="flex justify-between gap-4 items-start">
+					<h2 className='font-medium'>Brand Details</h2>
+					<div className='flex justify-between gap-4 items-start'>
 						<TextField
-							label="Brand"
-							type="text"
-							variant="outlined"
-							size="small"
+							label='Brand'
+							type='text'
+							variant='outlined'
+							size='small'
 							required
 							value={brand}
 							onChange={(e) => setBrand(e.target.value)}
 						/>
-						<div className="w-24 h-10 flex items-center justify-center border rounded-lg">
-							{!logoPreview ? <ImageIcon /> :
-								<img draggable="false" src={logoPreview} alt="Brand Logo" className="w-full h-full object-contain" />
-							}
+						<div className='w-24 h-10 flex items-center justify-center border rounded-lg'>
+							{!logoPreview ? (
+								<ImageIcon />
+							) : (
+								<img
+									draggable='false'
+									src={logoPreview}
+									alt='Brand Logo'
+									className='w-full h-full object-contain'
+								/>
+							)}
 						</div>
-						<label className="rounded bg-gray-400 text-center cursor-pointer text-white py-2 px-2.5 shadow hover:shadow-lg">
+						<label className='rounded bg-gray-400 text-center cursor-pointer text-white py-2 px-2.5 shadow hover:shadow-lg'>
 							<input
-								type="file"
-								name="logo"
-								accept="image/*"
+								type='file'
+								name='logo'
+								accept='image/*'
 								onChange={handleLogoChange}
-								className="hidden"
+								className='hidden'
 							/>
 							Choose Logo
 						</label>
 					</div>
-
 				</div>
 
-				<div className="flex flex-col gap-2 m-2 sm:w-1/2">
-					<h2 className="font-medium">Specifications</h2>
+				<div className='flex flex-col gap-2 m-2 sm:w-1/2'>
+					<h2 className='font-medium'>Specifications</h2>
 
-					<div className="flex justify-evenly gap-2 items-center">
-						<TextField value={specsInput.title} onChange={handleSpecsChange} name="title" label="Name" placeholder="Model No" variant="outlined" size="small" />
-						<TextField value={specsInput.description} onChange={handleSpecsChange} name="description" label="Description" placeholder="WJDK42DF5" variant="outlined" size="small" />
-						<span onClick={() => addSpecs()} className="py-2 px-6 bg-primary-blue text-white rounded hover:shadow-lg cursor-pointer">Add</span>
+					<div className='flex justify-evenly gap-2 items-center'>
+						<TextField
+							value={specsInput.title}
+							onChange={handleSpecsChange}
+							name='title'
+							label='Name'
+							placeholder='Model No'
+							variant='outlined'
+							size='small'
+						/>
+						<TextField
+							value={specsInput.description}
+							onChange={handleSpecsChange}
+							name='description'
+							label='Description'
+							placeholder='WJDK42DF5'
+							variant='outlined'
+							size='small'
+						/>
+						<span
+							onClick={() => addSpecs()}
+							className='py-2 px-6 bg-primary-blue text-white rounded hover:shadow-lg cursor-pointer'
+						>
+							Add
+						</span>
 					</div>
 
-					<div className="flex flex-col gap-1.5">
+					<div className='flex flex-col gap-1.5'>
 						{specs.map((spec, i) => (
-							<div className="flex justify-between items-center text-sm rounded bg-blue-50 py-1 px-2">
-								<p className="text-gray-500 font-medium">{spec.title}</p>
+							<div className='flex justify-between items-center text-sm rounded bg-blue-50 py-1 px-2'>
+								<p className='text-gray-500 font-medium'>{spec.title}</p>
 								<p>{spec.description}</p>
-								<span onClick={() => deleteSpec(i)} className="text-red-600 hover:bg-red-200 bg-red-100 p-1 rounded-full cursor-pointer">
+								<span
+									onClick={() => deleteSpec(i)}
+									className='text-red-600 hover:bg-red-200 bg-red-100 p-1 rounded-full cursor-pointer'
+								>
 									<DeleteIcon />
 								</span>
 							</div>
 						))}
 					</div>
 
-					<h2 className="font-medium">Product Images</h2>
-					<div className="flex gap-2 overflow-x-auto h-32 border rounded">
+					<h2 className='font-medium'>Product Images</h2>
+					<div className='flex gap-2 overflow-x-auto h-32 border rounded'>
 						{imagesPreview.map((image, i) => (
-							<img draggable="false" src={image} alt="Product" key={i} className="w-full h-full object-contain" />
+							<img
+								draggable='false'
+								src={image.url}
+								alt='Product'
+								key={image.url}
+								className='w-full h-full object-contain'
+							/>
 						))}
 					</div>
-					<label className="rounded font-medium bg-gray-400 text-center cursor-pointer text-white p-2 shadow hover:shadow-lg my-2">
+					<label className='rounded font-medium bg-gray-400 text-center cursor-pointer text-white p-2 shadow hover:shadow-lg my-2'>
 						<input
-							type="file"
-							name="images"
-							accept="image/*"
+							type='file'
+							name='images'
+							accept='image/*'
 							multiple
 							onChange={handleProductImageChange}
-							className="hidden"
+							className='hidden'
 						/>
 						Choose Files
 					</label>
 
-					<div className="flex justify-end">
-						<input form="mainform" type="submit" className="bg-primary-orange uppercase w-1/3 p-3 text-white font-medium rounded shadow hover:shadow-lg cursor-pointer" value="Submit" />
+					<div className='flex justify-end'>
+						<input
+							form='mainform'
+							type='submit'
+							className='bg-primary-orange uppercase w-1/3 p-3 text-white font-medium rounded shadow hover:shadow-lg cursor-pointer'
+							value='Submit'
+						/>
 					</div>
-
 				</div>
-
 			</form>
 		</>
 	);

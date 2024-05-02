dd71e1e680ebcab76d633443bfd815ccc033d5cb
Mohamad Anas <mohamadanas0501@gmail.com>
2024-05-02 20:11:14 +0530

    Removed Padding and Hidden CSS

--- src/components/Layouts/Categories.jsx
+++ src/components/Layouts/Categories.jsx
@@ -23,40 +23,24 @@ const catNav = [
 		icon: electronics,
 	},
 	{
-		name: "Home",
-		icon: home,
-	},
-	{
-		name: "Travel",
-		icon: travel,
-	},
-	{
 		name: "Appliances",
 		icon: appliances,
 	},
 	{
-		name: "Furniture",
-		icon: furniture,
-	},
-	{
-		name: "Beauty,Toys & more",
+		name: "Beauty & Toys",
 		icon: beauty,
-	},
-	{
-		name: "Grocery",
-		icon: grocery,
 	},
 ]
 
 const Categories = () => {
 	return (
-		<section className="hidden sm:block bg-white mt-10 mb-4 min-w-full px-12 py-1 shadow overflow-hidden">
+		<section className="sm:block bg-white mt-10 mb-4 min-w-full  py-1 shadow overflow-hidden">
 
 			<div className="flex items-center justify-between mt-4">
 
 				{catNav.map((item, i) => (
-					<Link to={`/products?category=${item.name}`} className="flex flex-col gap-1 items-center p-2 group" key={i}>
-						<div className="h-16 w-16">
+					<Link to={`/products?category=${item.name}`} className="flex flex-col gap-1 items-center p-1 group" key={i}>
+						<div className="h-10 w-10">
 							<img draggable="false" className="h-full w-full object-contain" src={item.icon} alt={item.name} />
 						</div>
 						<span className="text-sm text-gray-800 font-medium group-hover:text-primary-blue">{item.name}</span>
